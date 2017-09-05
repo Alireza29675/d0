4f42e302ac3fcbfd988d9b848582c13f45baca76
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-09-06 00:06:12 +0430

    JSDoc descriptions added to every method of D0

--- src/index.js
+++ src/index.js
@@ -1,97 +1,179 @@
 class D0 {
     constructor () {
-        this.dimensions = []
-        for (let i in arguments) this.dimensions.push(arguments[i])
+        this.dimensions = [];
+        for (let arg of arguments) this.dimensions.push(arg)
     }
 
-    static ZERO = new D0()
+    /**
+     * Zero point in every dimension
+     * @type {D0}
+     */
+    static ZERO = new D0();
 
-    // Famous dimensions
-    d (n, to) {
-        if (to === undefined) return this.dimensions[n-1] || 0
-        else if (n !== undefined) {
-            this.dimensions[n-1] = to
-            while (this.dimensions[this.dimensions.length-1] === 0) this.dimensions.splice(this.dimensions.length-1, 1)
+
+    /**
+     * sets or gets dimension-n
+     * @param {Number} n - number of dimension
+     * @param {Number=} setTo - set value for dimension-n
+     * @returns {Number|D0}
+     */
+    d (n, setTo) {
+        if (!setTo) return this.dimensions[n-1] || 0;
+        this.dimensions[n-1] = setTo;
+        while (this.dimensions[this.dimensions.length-1] === 0) {
+            this.dimensions.splice(this.dimensions.length-1, 1) // removing latest zero dimensions
         }
         return this
     }
+
+
+    /**
+     * returns dimensions count
+     * @returns {Number}
+     */
     get count () {
         return this.dimensions.length
     }
-    get x () {
-        return this.d(1) || 0 
-    }
-    set x (to) {
-        this.d(1, to) 
-    }
-    get y () {
-        return this.d(2) || 0 
-    }
-    set y (to) {
-        this.d(2, to) 
-    }
-    get z () {
-        return this.d(3) || 0 
-    }
-    set z (to) {
-        this.d(3, to) 
-    }
-    get w () {
-        return this.d(4) || 0 
-    }
-    set w (to) {
-        this.d(4, to) 
-    }
 
-    // methods
+
+    /**
+     * gets famous dimensions (x, y, z, w)
+     * @returns {Number}
+     */
+    get x () { return this.d(1) || 0  }
+    get y () { return this.d(2) || 0  }
+    get z () { return this.d(3) || 0  }
+    get w () { return this.d(4) || 0  }
+
+
+    /**
+     * sets famous dimensions (x, y, z, w)
+     * @param {Number} to - set value for dimension
+     */
+    set x (to) { this.d(1, to)  }
+    set y (to) { this.d(2, to)  }
+    set z (to) { this.d(3, to)  }
+    set w (to) { this.d(4, to)  }
+
+
+    /**
+     * iterate over every dimension of point
+     * @param {Function} func - function calls with this format (index, value)
+     * @returns {D0}
+     */
     each (func) {
         for (let i = 1; i <= this.count; i++) func(i, this.d(i))
         return this
     }
-    eachWith (point, func) {
-        const maxCount = Math.max(this.count, point.count)
-        for (let i = 1; i <= maxCount; i++) func(i, this.d(i), point.d(i))
+
+
+    /**
+     * iterate over every dimension of two points and pass their dimensions side by side
+     * @param secondPoint - another point which wants to be compared with
+     * @param func - function calls with this format (index, valueOfPoint1, valueOfPoint2)
+     * @returns {D0}
+     */
+    eachWith (secondPoint, func) {
+        const maxCount = Math.max(this.count, secondPoint.count)
+        for (let i = 1; i <= maxCount; i++) func(i, this.d(i), secondPoint.d(i))
         return this
     }
+
+
+    /**
+     * adds a point to this point
+     * @param {D0} point
+     * @returns {D0}
+     */
     add (point) {
-        return this.eachWith(point, (index, i, j) => this.d(index, i + j))
+        for (let point of arguments) this.eachWith(point, (index, i, j) => this.d(index, i + j))
+        return this
     }
-    sub (point) {
-        return this.eachWith(point, (index, i, j) => this.d(index, i - j))
+
+
+    /**
+     * subtract a point from this point
+     * @param {D0} point
+     * @returns {D0}
+     */
+    subtract (point) {
+        for (let point of arguments) this.eachWith(point, (index, i, j) => this.d(index, i - j))
+        return this
     }
+
+
+    /**
+     * multiply a point or a number to this point
+     * @param {D0|Number} toWhat
+     * @returns {D0}
+     */
+    multiply (toWhat) {
+        for (let toWhat of arguments) {
+            if (toWhat instanceof D0) this.eachWith(toWhat, (index, i, j) => this.d(index, i * j));
+            else this.each((index, i) => this.d(index, i * toWhat))
+        }
+        return this
+    }
+
+
+    /**
+     * divide a point or a number from this point
+     * @param {D0|Number} fromWhat
+     * @returns {D0}
+     */
+    divide (fromWhat) {
+        for (let fromWhat of arguments) {
+            if (fromWhat instanceof D0) this.eachWith(fromWhat, (index, i, j) => this.d(index, i / j));
+            else this.each((index, i) => this.d(index, i / fromWhat))
+        }
+        return this
+    }
+
+
+    /**
+     * checks equality of this point and one another point
+     * @param {D0} point
+     * @returns {boolean}
+     */
     equals (point) {
-        let ret = true
-        this.eachWith(point, (index, i, j) => {
-            if (i !== j) ret = false
-        })
-        return ret
+        let ret = true;
+        this.eachWith(point, (index, i, j) => ret &= (i === j));
+        return Boolean(ret)
     }
+
+
+    /**
+     * absolutes every dimension of point
+     * @returns {D0}
+     */
     abs () {
         return this.each((index, i) => this.d(index, Math.abs(this.d(i))))
     }
+
+
+    /**
+     * returns distance between this point and one another point
+     * @param {D0} point
+     * @returns {number}
+     */
     distance (point) {
-        let count = 0, sumSqr = 0
+        let sumSqr = 0;
         this.eachWith(point, (index, i, j) => {
-            count++
             sumSqr += Math.pow(i - j, 2)
-        })
+        });
         return Math.sqrt(sumSqr)
     }
-    multiplay (toWhat) {
-        if (toWhat instanceof D0) return this.eachWith(toWhat, (index, i, j) => this.d(index, i * j))
-        return this.each((index, i) => this.d(index, i * toWhat))
-    }
-    divide (fromWhat) {
-        if (fromWhat instanceof D0) return this.eachWith(fromWhat, (index, i, j) => this.d(index, i / j))
-        return this.each((index, i) => this.d(index, i / fromWhat))
-    }
 
-    // to String Point
+
+    /**
+     * returns string view of this point
+     * @returns {string}
+     */
     toString () {
-        let ret = "("
+        let ret = "(";
         for (let d of this.dimensions) ret += (d + ", ")
         return ret.slice(0, -2) + ")"
     }
 }
 
-module.exports = D0
+module.exports = D0;
