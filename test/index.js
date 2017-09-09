e0c37664c30ae1499c8243c9fb5e6accd631da29
Hamifthi <hamedonil@yahoo.com>
2017-09-09 22:47:14 +0430

    few changes

--- test/index.js
+++ test/index.js
@@ -1,8 +1,19 @@
 let D0 = require('../lib/index');
 
+let zero = D0.ZERO;
+
 let point1 = new D0(3, 4, 5);
 let point2 = new D0(3, 2, 2);
 let point3 = point1.clone().d(2, 7).d(3, 2);
+
+console.log(point1.d(1))
+
+point1.w = 6
+console.log(point1.w)
+
+console.log(point1.each(function(index, value){
+    console.log(value)
+}))
 
 console.log('Distance between point1 and point2:');
 console.log(
